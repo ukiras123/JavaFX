fbf0a23def84f7fd88f6d1cf85ec3848634e2cb2
ukiras123 <ukiras@gmail.com>
2017-05-29 05:57:57 -0700

    adding git ignore file

--- src/main/java/com/kiran/controllers/SignupController.java
+++ src/main/java/com/kiran/controllers/SignupController.java
@@ -3,9 +3,11 @@ package com.kiran.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by Kiran on 5/29/17.
@@ -19,9 +21,28 @@ public class SignupController {
     @FXML public Button signUpButton;
     @FXML public Button signUpCancel;
 
+    @FXML public Label firstNameError;
+    @FXML public Label lastNameError;
+    @FXML public Label userNameError;
+    @FXML public Label passwordError;
+
+
     @FXML
     protected void cancelEvent(ActionEvent event) {
         System.out.println("Cancel");
         ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
+
+    @FXML
+    protected void signUpEvent(ActionEvent event) {
+        System.out.println("Signup");
+        String firstName = signUpFirstName.getText();
+        String lastName = signUpLastName.getText();
+        String userName = signUpUserName.getText();
+        String password = signUpPassword.getText();
+
+        if (StringUtils.isEmpty(firstName) || StringUtils.isEmpty(lastName) || StringUtils.isEmpty(userName) || StringUtils.isEmpty(password)) {
+            //firstNameError.setValue("All Field Mandatory");
+        }
     }
 }
